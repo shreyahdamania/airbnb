0922a0301b9c1f0314972077c47044bc53eeaaf9
Shreyash <144013509+shreyahdamania@users.noreply.github.com>
2026-03-27 19:17:54 +0530

    Added new columns to user model and attached picture for every user, seeded database with fake data, connected user review frontend with backend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_03_10_135519) do
+ActiveRecord::Schema[8.0].define(version: 2026_03_27_102603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -119,6 +119,12 @@ ActiveRecord::Schema[8.0].define(version: 2026_03_10_135519) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "address_1"
+    t.string "address_2"
+    t.string "city"
+    t.string "state"
+    t.string "country"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
